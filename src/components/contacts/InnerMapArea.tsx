9bf34537b16efa8a6ffee52a5ecc531a8e5aea86
balaprem678 <balaprem678@gmail.com>
2026-03-30 20:56:15 +0530

    chnages

--- src/components/contacts/InnerMapArea.tsx
+++ src/components/contacts/InnerMapArea.tsx
@@ -10,7 +10,11 @@ const InnerMapArea = () => {
                         <span><Image src={mapIcon} alt="contact image" /></span>
                     </div>
                 </div>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193596.26002818075!2d-74.1443121872927!3d40.69728463485858!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1745055504744!5m2!1sen!2sbd" width="600" height="450" style={{ border: 0 }} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+               
+
+
+
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3886.6042264474518!2d80.27275357507806!3d13.060845387262779!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a526703186886ef%3A0xc96617a758dc125a!2sNOORYAK%20TECHNOLOGIES%20Website%20Development%20Company%20Chennai!5e0!3m2!1sen!2sin!4v1774884227172!5m2!1sen!2sin" width="600" height="450" style={{ border: 0 }} allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
     );
