07ba45ab3ca33c8cc7a1848e857965ffb8497cc4
Prem <balaprem678@gmail.com>
2026-04-02 18:59:38 +0530

    changes

--- src/data/testimonialData.ts
+++ src/data/testimonialData.ts
@@ -1,9 +1,5 @@
 import { TestimonialDT } from "@/types/testimonial-d-t";
-import avaterOne from "../../public/assets/images/home/client1.png";
-import avaterTwo from "../../public/assets/images/home/client2.png";
-import avaterThree from "../../public/assets/images/home/client3.png";
-import avaterFour from "../../public/assets/images/home/client4.png";
-import avaterFive from "../../public/assets/images/home/client5.png";
+import { Images } from "@/utils/Images";
 import avaterSix from "../../public/assets/img/avater/avater-6.jpg";
 //home four avater image
 import avater7 from "../../public/assets/img/home-04/avater/avater-1.jpg";
@@ -35,7 +31,7 @@ const testimonialsData: TestimonialDT[] = [
         quote: "“We recently partnered with Nooryak Technologies, the leading Ecommerce Website Development Company in Chennai, to design our Luxxrr online store. Though some final refinements are still in progress, their exceptional work quality, responsive support, and commitment to excellence have already impressed us so much that we felt compelled to share this happy review. The stunning, user-friendly ecommerce website they delivered perfectly captures our brand vision, making them truly stand out in the industry.”",
         name: "Hitesh Cosy",
         position: "Manger",
-        avatar: avaterTwo,
+        avatar: Images.avaterTwo,
         style: "white-style"
     },
     {
@@ -43,7 +39,7 @@ const testimonialsData: TestimonialDT[] = [
         quote: "“If you’re looking for a reliable Web Development Company, Nooryak Technologies delivers top-quality websites that perform and look amazing. Just How we got a brand new looking professional website before a couple of Months. Thanks.”",
         name: "Aswini's Cibil Agency",
         position: "CEO",
-        avatar: avaterOne,
+        avatar: Images.avaterOne,
         style: "green-style"
     },
     {
@@ -51,7 +47,7 @@ const testimonialsData: TestimonialDT[] = [
         quote: "“I recently worked with Nooryak Technologies to develop my ecommerce brand, CosyChic, and I couldn't be more thrilled with the results. They are truly the best website designing agency in Chennai. Their expertise in ecommerce website development, especially in Triplicane, is exceptional. The website they designed for us is stunning, and their video editing works are equally impressive. I highly recommend Nooryak Tech for anyone looking for top-notch ecommerce development in Chennai. Thank you for bringing our vision to life!”",
         name: "Hitesh Mali",
         position: "Managing Director",
-        avatar: avaterTwo,
+        avatar: Images.avaterTwo,
         style: "black-style"
     },
     {
@@ -59,15 +55,15 @@ const testimonialsData: TestimonialDT[] = [
         quote: "“I am thoroughly impressed with the amazing and great work done by Nooryak Technologies on our affiliate e-commerce website, Hish Gadgets! As the best website development company in chennai and the best e-commerce designing agency, they delivered a sleek, user-friendly design that perfectly captures our brand's essence. Highly recommended their services!”",
         name: "Hisham Hiba",
         position: "CEO & Founder",
-        avatar: avaterTwo,
+        avatar: Images.avaterTwo,
         style: "white-style"
     },
     {
         id: 5,
         quote: "“Nooryak Technologies is the best Website Designing Company I’ve worked with — they built a stunning site for my business and boosted my sales massively.”",
         name: "MOHAMMED YUSUF.M",
-         position: "Manger",
-        avatar: avaterThree,
+        position: "Manger",
+        avatar: Images.avaterThree,
         style: "grey-style"
     },
     {
@@ -75,7 +71,7 @@ const testimonialsData: TestimonialDT[] = [
         quote: "“Nooryak Technologies is a Professional Marketing And Web Development Company in Chennai based in Triplicane. Its always unique for its services and gives most greater results for startups, entrepreneurs and business owners.”",
         name: "Ameen Noor",
         position: "CEO & Founder",
-        avatar: avaterFour,
+        avatar: Images.avaterFour,
         style: "green-style"
     },
     {
@@ -83,7 +79,7 @@ const testimonialsData: TestimonialDT[] = [
         quote: "“Best trusted marketing and web development agency in Chennai for startups and entrepreneurs. I suggest Nooryak Technologies for all who seek to empower their business online.”",
         name: "suriyakumar devaraj",
         position: "CEO & Founder",
-        avatar: avaterTwo,
+        avatar: Images.avaterTwo,
         style: "black-style"
     },
     //home four testimonial data start
--- src/utils/Images.tsx
+++ src/utils/Images.tsx
@@ -1,6 +1,5 @@
 // Common Images Import Here
 import logo from "../../public/assets/images/common/Logo.png";
-import Logo_blue from "../../public/assets/images/common/Logo-blue.png";
 import gmail from "../../public/assets/images/common/gmail.png";
 import instagram from "../../public/assets/images/common/instagram.png";
 import whatsapp from "../../public/assets/images/common/whatsapp.png";
@@ -12,57 +11,66 @@ import trustpilot from "../../public/assets/images/common/trustpilot.png";
 import mobile from "../../public/assets/images/mobile.png";
 
 // Home Images Import Here
-import herobanner from "../../public/assets/images/home/herobanner.jpeg";
+import herobanner from "../../public/assets/images/home/herobanner.webp";
 import herobanner1 from "../../public/assets/images/home/herobanner.jpg";
-import herobanner_person from "../../public/assets/images/home/herobanner_person.png";
+import herobanner_person from "../../public/assets/images/home/herobanner_person.webp";
 import about_1 from "../../public/assets/images/home/about-1.webp";
 import about_2 from "../../public/assets/images/home/about-2.webp";
-import abt_sec from "../../public/assets/images/home/abt_sec.png";
+import abt_sec from "../../public/assets/images/home/abt_sec.webp";
 
-import services_1 from "../../public/assets/images/home/services_1.png";
-import services_2 from "../../public/assets/images/home/services_2.png";
-import services_3 from "../../public/assets/images/home/services_3.png";
-import services_4 from "../../public/assets/images/home/services_4.png";
+import services_1 from "../../public/assets/images/home/services_1.webp";
+import services_2 from "../../public/assets/images/home/services_2.webp";
+import services_3 from "../../public/assets/images/home/services_3.webp";
+import services_4 from "../../public/assets/images/home/services_4.webp";
 
-import craftingwebsite_banner from "../../public/assets/images/home/craftingwebsite_banner.png";
-import why_choose_card_icon_1 from "../../public/assets/images/home/why_choose_card_icon_1.png";
-import why_choose_card_icon_2 from "../../public/assets/images/home/why_choose_card_icon_2.png";
-import why_choose_card_icon_3 from "../../public/assets/images/home/why_choose_card_icon_3.png";
-import why_choose_card_icon_4 from "../../public/assets/images/home/why_choose_card_icon_4.png";
+import craftingwebsite_banner from "../../public/assets/images/home/craftingwebsite_banner.webp";
+import why_choose_card_icon_1 from "../../public/assets/images/home/why_choose_card_icon_1.webp";
+import why_choose_card_icon_2 from "../../public/assets/images/home/why_choose_card_icon_2.webp";
+import why_choose_card_icon_3 from "../../public/assets/images/home/why_choose_card_icon_3.webp";
+import why_choose_card_icon_4 from "../../public/assets/images/home/why_choose_card_icon_4.webp";
 
-import project_1 from "../../public/assets/images/home/project_1.svg";
-import project_2 from "../../public/assets/images/home/project_2.svg";
-import project_3 from "../../public/assets/images/home/project_3.svg";
-import project_4 from "../../public/assets/images/home/project_4.svg";
-import project_5 from "../../public/assets/images/home/project_5.svg";
-import project_6 from "../../public/assets/images/home/project_6.svg";
+import project_1 from "../../public/assets/images/home/project_1.webp";
+import project_2 from "../../public/assets/images/home/project_2.webp";
+import project_3 from "../../public/assets/images/home/project_3.webp";
+import project_4 from "../../public/assets/images/home/project_4.webp";
+import project_5 from "../../public/assets/images/home/project_5.webp";
+import project_6 from "../../public/assets/images/home/project_6.webp";
 
-import clientlogo_1 from "../../public/assets/images/client_logo/clientlogo_1.png";
-import clientlogo_2 from "../../public/assets/images/client_logo/clientlogo_2.png";
-import clientlogo_3 from "../../public/assets/images/client_logo/clientlogo_3.png";
-import clientlogo_4 from "../../public/assets/images/client_logo/clientlogo_4.png";
-import clientlogo_5 from "../../public/assets/images/client_logo/clientlogo_5.png";
-import clientlogo_6 from "../../public/assets/images/client_logo/clientlogo_6.png";
-import clientlogo_7 from "../../public/assets/images/client_logo/clientlogo_7.png";
-import clientlogo_8 from "../../public/assets/images/client_logo/clientlogo_8.png";
-import clientlogo_9 from "../../public/assets/images/client_logo/clientlogo_9.png";
-import clientlogo_10 from "../../public/assets/images/client_logo/clientlogo_10.png";
-import clientlogo_11 from "../../public/assets/images/client_logo/clientlogo_11.png";
-import clientlogo_12 from "../../public/assets/images/client_logo/clientlogo_12.png";
-import clientlogo_13 from "../../public/assets/images/client_logo/clientlogo_13.png";
-import clientlogo_14 from "../../public/assets/images/client_logo/clientlogo_14.png";
-import clientlogo_15 from "../../public/assets/images/client_logo/clientlogo_15.png";
-import clientlogo_16 from "../../public/assets/images/client_logo/clientlogo_16.png";
-import clientlogo_17 from "../../public/assets/images/client_logo/clientlogo_17.png";
-import clientlogo_18 from "../../public/assets/images/client_logo/clientlogo_18.png";
-import clientlogo_19 from "../../public/assets/images/client_logo/clientlogo_19.png";
+import clientlogo_1 from "../../public/assets/images/client_logo/clientlogo_1.webp";
+import clientlogo_2 from "../../public/assets/images/client_logo/clientlogo_2.webp";
+import clientlogo_3 from "../../public/assets/images/client_logo/clientlogo_3.webp";
+import clientlogo_4 from "../../public/assets/images/client_logo/clientlogo_4.webp";
+import clientlogo_5 from "../../public/assets/images/client_logo/clientlogo_5.webp";
+import clientlogo_6 from "../../public/assets/images/client_logo/clientlogo_6.webp";
+import clientlogo_7 from "../../public/assets/images/client_logo/clientlogo_7.webp";
+import clientlogo_8 from "../../public/assets/images/client_logo/clientlogo_8.webp";
+import clientlogo_9 from "../../public/assets/images/client_logo/clientlogo_9.webp";
+import clientlogo_10 from "../../public/assets/images/client_logo/clientlogo_10.webp";
+import clientlogo_11 from "../../public/assets/images/client_logo/clientlogo_11.webp";
+import clientlogo_12 from "../../public/assets/images/client_logo/clientlogo_12.webp";
+import clientlogo_13 from "../../public/assets/images/client_logo/clientlogo_13.webp";
+import clientlogo_14 from "../../public/assets/images/client_logo/clientlogo_14.webp";
+import clientlogo_15 from "../../public/assets/images/client_logo/clientlogo_15.webp";
+import clientlogo_16 from "../../public/assets/images/client_logo/clientlogo_16.webp";
+import clientlogo_17 from "../../public/assets/images/client_logo/clientlogo_17.webp";
+import clientlogo_18 from "../../public/assets/images/client_logo/clientlogo_18.webp";
+import clientlogo_19 from "../../public/assets/images/client_logo/clientlogo_19.webp";
+
+
+import avaterOne from "../../public/assets/images/home/client1.webp";
+import avaterTwo from "../../public/assets/images/home/client2.webp";
+import avaterThree from "../../public/assets/images/home/client3.webp";
+import avaterFour from "../../public/assets/images/home/client4.webp";
+import avaterFive from "../../public/assets/images/home/client5.webp";
 
 export const Images = {
 
     // Common Images Import Here
-    logo, Logo_blue, gmail, instagram, whatsapp, twitter, facebook, linkedin, youtube, mobile, trustpilot,
+    logo, gmail, instagram, whatsapp, twitter, facebook, linkedin, youtube, mobile, trustpilot,
 
     // Home Images Import here
-    herobanner, herobanner1, herobanner_person, about_1, about_2, abt_sec, services_1, services_2, services_3, services_4, craftingwebsite_banner, why_choose_card_icon_1, why_choose_card_icon_2, why_choose_card_icon_3, why_choose_card_icon_4, project_1, project_2, project_3, project_4, project_5, project_6, clientlogo_1, clientlogo_2, clientlogo_3, clientlogo_4, clientlogo_5, clientlogo_6, clientlogo_7, clientlogo_8, clientlogo_9, clientlogo_10, clientlogo_11, clientlogo_12, clientlogo_13, clientlogo_14, clientlogo_15, clientlogo_16, clientlogo_17, clientlogo_18, clientlogo_19
+    herobanner, herobanner1, herobanner_person, about_1, about_2, abt_sec, services_1, services_2, services_3, services_4, craftingwebsite_banner, why_choose_card_icon_1, why_choose_card_icon_2, why_choose_card_icon_3, why_choose_card_icon_4, project_1, project_2, project_3, project_4, project_5, project_6, clientlogo_1, clientlogo_2, clientlogo_3, clientlogo_4, clientlogo_5, clientlogo_6, clientlogo_7, clientlogo_8, clientlogo_9, clientlogo_10, clientlogo_11, clientlogo_12, clientlogo_13, clientlogo_14, clientlogo_15, clientlogo_16, clientlogo_17, clientlogo_18, clientlogo_19,
+
+    avaterOne, avaterTwo, avaterThree, avaterFour, avaterFive,
 
 };
